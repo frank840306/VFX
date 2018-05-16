1d09f38b43bd1f774e1e007a2950b6301a9fab80
Anita Chen <b02902044@ntu.edu.tw>
2018-05-16 14:51:12 +0800

    fix bugs in constructDescriptor.m

--- hw2/src/MSOP.m
+++ hw2/src/MSOP.m
@@ -3,7 +3,7 @@ function [feature] = MSOP(image)
     [row, col, value] = localMax(image);
     descriptor = constructDescriptor(image, row, col);
 %     level = ones(length(row), 1);
-    for i = 1:3
+    for i = 1:2
         n = 2 ^ i;
 %         disp(['Level: ' int2str(i) ', subsampling factor: ' int2str(n)]);
         image_tmp = image(1:n:end, 1:n:end);
@@ -25,15 +25,12 @@ function [feature] = MSOP(image)
     dist = zeros(length(row));
 %     disp(['Number of features before non-maximal suppression: ' int2str(length(row))]);
     for i = 1:length(row)
-        index = find(value * 0.95 > value(i));
+        index = find(value > value(i));
         if isempty(index)
             dist(i) = Inf;
         else
             dist(i) = min(dist2([row(i), col(i)], [row(index), col(index)]));
         end
-%         if mod(i, 1000) == 0
-%             disp(['Computing the radius of the ' int2str(i) 'th point']);
-%         end
     end
 
     n = 500;
--- hw2/src/constructDescriptor.m
+++ hw2/src/constructDescriptor.m
@@ -14,25 +14,30 @@ function [descriptor] = constructDescriptor(image, row, col)
     index = isnan(S) | isinf(S);
     S(index) = 0;
 
-    % theta = angleCalc(S, C);
     index = sub2ind(size(C), row_int, col_int);
-    rotation = [C(index), -S(index), S(index), C(index)];
+%     rotation = [C(index), -S(index), S(index), C(index)];
+    rotation = [S(index), -C(index), C(index), S(index)];
     rotation = transpose(rotation);
     tmp = size(rotation);
-%     disp(tmp(2));
     rotation = reshape(rotation, 2, 2, tmp(2));
-%     disp(size(rotation));
 
     % patch = 40 * 40, and the feature point is at (20, 20)
     window = zeros(2, 1600);
     descriptor = zeros(length(row_int), 64);
     cell(1:8) = 5;
     for i = 1:length(row_int)
+        % prepare window
         window(1, :) = reshape(repmat((col_int(i) - 19:col_int(i) + 20), [40, 1]), 1, 1600);
         window(2, :) = repmat((row_int(i) - 19:row_int(i) + 20), [1, 40]);
-        window(:, :) = rotation(i) * window;
-        window(1, :) = window(1, :) + (col_int(i) - window(1, 761));
-        window(2, :) = window(2, :) + (row_int(i) - window(2, 20));
+        
+        % rotate window
+        window(:, :) = rotation(:, :, i) * window;
+        window(1, :) = round(window(1, :));
+        window(2, :) = round(window(2, :));
+        
+        %shift window
+        window(1, :) = window(1, :) + (col_int(i) - window(1, 780));
+        window(2, :) = window(2, :) + (row_int(i) - window(2, 780));
         index = sub2ind(size(image), round(window(2, :)), round(window(1, :)));
         patch = reshape(image(index), 40, 40);
         C = mat2cell(patch, cell, cell);
@@ -40,3 +45,7 @@ function [descriptor] = constructDescriptor(image, row, col)
             descriptor(i, j) = mean(mean(C{j}));
         end
     end
+    M = mean(descriptor, 2);
+    S = std(descriptor, 0, 2);
+    descriptor = (descriptor - M) ./ S;
+%     disp(size(descriptor));
